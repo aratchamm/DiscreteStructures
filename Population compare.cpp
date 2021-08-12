d61a1d6008420e22a1b57d09bce2bf1a090f1f16
mm <aratcha.mm@gmail.com>
2021-08-12 23:57:31 +0700

    add file

--- Population compare.cpp
+++ Population compare.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 main(){
-	float pA,rA,pB,rB,A_n[3],B_n[3];
+	float pA,rA,pB,rB,A_n[100],B_n[100];
 	int a;
-	int x,y=0;
+	int x,y;
+	int Ay,By;
 	printf("Population of A = ");
 	scanf("%f",&pA);
 	printf("Growth rate (%%) = ");
@@ -13,8 +14,11 @@ main(){
 	printf("Growth rate (%%) = ");
 	scanf("%f",&rB);
 	
-	for(int loop=0; loop<4; loop++) {
-		for(int i=0; i<4; i++) {
+	x = pA; 
+	y = pB;
+	
+	while(x<=y) {
+		for(int i=0; i<100; i++) {
 			if(i==0) {
 				A_n[i] = pA;
 				B_n[i] = pB;
@@ -23,12 +27,14 @@ main(){
 				A_n[i] = A_n[i-1]*(rA*0.01) + A_n[i-1] ;
 				B_n[i] = B_n[i-1]*(rB*0.01) + B_n[i-1] ;
 			}
-			//A_n[i] = x; 
-			//B_n[i] = y;
-			//printf("%d%d",Ay,By);
-		}
-		printf("year %d, A = %.2f B = %.2f\n",loop,A_n[loop],B_n[loop]);
+			x = A_n[i]; 
+			y = B_n[i];
+		
+		printf("year %d, A = %.2f B = %.2f\n",a,A_n[i],B_n[i]);
 		a++;
+		if(A_n[i]>B_n[i])break;
 	}
 	printf("During = %d Years", a-1);
+	}
 }
+	
