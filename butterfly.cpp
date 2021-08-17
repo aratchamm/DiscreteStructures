5aa509fb75995ae3d526883e783a7e9240248633
mm <aratcha.mm@gmail.com>
2021-08-17 23:16:09 +0700

    add file

---
+++ butterfly.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main() {
+	int num,middle,row,space,sym;
+	scanf("%d",&num);
+	for(row=num; row>= 1; row--) {
+		for(sym=num-1; sym>=row; sym--)printf("*");
+		for(space=1; space<=(2*row)-1; space++)printf(" ");
+		for(sym=num-1; sym>=row; sym--)printf("*");
+		printf("\n");
+	}
+		
+	for(middle=1; middle<=num+(num-1); middle++)printf("*");
+	printf("\n");
+	
+	for(row=1; row<= num; row++) {
+	for(sym=1; sym<= num-row; sym++)printf("*");
+	for(space=1; space<= (2*row)-1; space++)printf(" ");
+	for(sym=1; sym<= num-row; sym++)printf("*");
+	printf("\n");
+}
+	
+	
+			
+	return 0;
+} 
+
+
