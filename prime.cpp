ad72d1ea1b1bb6b36a6628816aa3acdf722d959b
mm <aratcha.mm@gmail.com>
2021-08-31 13:36:20 +0700

    add file

---
+++ prime.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int isPrime(int);
+int i,b;
+int main() 
+{
+	int x;
+	scanf("%d", &x);
+	if (isPrime(x) == 1)
+		printf("Prime number");
+	else
+		printf("Not Prime number");
+	return 0;
+}
+int isPrime(int a)
+{
+	if(a==1)return 0;
+	for(i=2; i<a; i++) {
+		if(a%i==0)return 0;
+	}
+	return 1;
+	
+}
+
+
