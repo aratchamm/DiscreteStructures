ff8beb0cceb3009180dcc1edc1d9856d74640c8c
mm <aratcha.mm@gmail.com>
2021-09-01 13:46:43 +0700

    edit file

--- prime_fx.cpp
+++ prime_fx.cpp
@@ -6,21 +6,22 @@ int main()
 {
 	int x;
 	scanf("%d", &x);
-	
-	if (x==1 || x==2) {
+	if (isPrime(x) == 1)
+		printf("Prime number");
+	else if (isPrime(x) == 2)return 0;
+	else
 		printf("Not Prime number");
-	}
-	else if (x==-99) {
-		return 0;
-	}
-	else for(i=2; i<x; i++) {
-		if(x%i==0) {
-			printf("Not Prime number");break;
-		}
-		else printf("Prime number");break;
-	}
-
 	return 0;
+}
+int isPrime(int a)
+{
+	if(a==1)return 0;
+	if(a==-99)return 2;
+	for(i=2; i<a; i++) {
+		if(a%i==0)return 0;
+	}
+	return 1;
+	
 }
 
 
