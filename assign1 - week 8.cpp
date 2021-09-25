c12c36828ce1cd5b57dac3a4796015af98198dcc
aratchamm <aratcha.mm@gmail.com>
2021-09-26 00:16:55 +0700

    add file

---
+++ assign1 - week 8.cpp
@@ -0,0 +1,42 @@
+#include <windows.h>
+#include <time.h>
+#include<stdio.h>
+
+
+#define screen_x 80
+#define screen_y 25
+HANDLE wHnd;
+CHAR_INFO consoleBuffer[screen_x * screen_y];
+COORD bufferSize = { screen_x,screen_y };
+COORD characterPos = { 0,0 };
+SMALL_RECT windowSize = { 0,0,screen_x - 1,screen_y - 1 };
+int setConsole(int x, int y)
+{
+	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+	SetConsoleScreenBufferSize(wHnd, bufferSize);
+	return 0;
+}
+void fill_data_to_buffer()
+{
+	for (int y = 0; y < screen_y; ++y) {
+		for (int x = 0; x < screen_x; ++x) {
+			consoleBuffer[x + screen_x * y].Char.AsciiChar = 'A' + (rand() % 26);
+			consoleBuffer[x + screen_x * y].Attributes = 0 + (rand() % 10);
+		}
+	}
+}
+void fill_buffer_to_console()
+{
+	WriteConsoleOutputA(wHnd, consoleBuffer, bufferSize, characterPos,
+		&windowSize);
+}
+int main()
+{
+	srand(time(NULL));
+	setConsole(screen_x, screen_y);
+	fill_data_to_buffer();
+	fill_buffer_to_console();
+	Sleep(5000);
+	return 0;
+}
