a5a5e28c1b41d8236edadc4af31e54af112fc857
aratchamm <aratcha.mm@gmail.com>
2021-09-27 22:58:51 +0700

    add file

---
+++ calculator grade.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+	
+	char str[50];
+	float credits;
+	float grade;
+	
+	printf("Subject = ");
+	scanf("%s", str);
+	printf("Credits = ");
+	scanf("%f", &credits);
+	printf("Grade = ");
+	scanf("%f", &grade);
+	
+	printf("Subject = %s | Credits = %.2f | Grade = %.2f",str,credits,grade);
+	
+}
